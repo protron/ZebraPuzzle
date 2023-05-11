7647aa521af813766d85a444916936feb68376e2
Mariano Desanze <protronm@gmail.com>
2023-05-11 01:35:50 -0300

    Refactor PermutationsCount & Solution for PermutationIndices

--- ZebraPuzzle/AllPermutations.cs
+++ ZebraPuzzle/AllPermutations.cs
@@ -7,5 +7,12 @@
         public static IList<IList<Pet>> PetsSets = Permutations.FromEnum<Pet>().ToArray();
         public static IList<IList<Drink>> DrinksSets = Permutations.FromEnum<Drink>().ToArray();
         public static IList<IList<Smoke>> SmokesSets = Permutations.FromEnum<Smoke>().ToArray();
+
+        public static int PermutationsCount = 
+            ColorsSets.Count == NationalitiesSets.Count &&
+            ColorsSets.Count == PetsSets.Count &&
+            ColorsSets.Count == DrinksSets.Count &&
+            ColorsSets.Count == SmokesSets.Count
+            ? ColorsSets.Count : throw new InvalidOperationException("All sets should match in size");
     }
 }
--- ZebraPuzzle/Solution.cs
+++ ZebraPuzzle/Solution.cs
@@ -1,71 +1,83 @@
 ﻿namespace ZebraPuzzle
 {
-    public record CombinationNumbers(
-        int ColorIndex = 0,
-        int NationalityIndex = 0,
-        int PetIndex = 0,
-        int DrinkIndex = 0,
-        int SmokeIndex = 0);
-
-    public record Solution(int Level, CombinationNumbers Numbers, Hypothesis H1, Hypothesis H2, Hypothesis H3, Hypothesis H4, Hypothesis H5)
+    public record Solution
     {
-        public static Solution BuildInitial() => new Solution(0, new CombinationNumbers(), new(1), new(2), new(3), new(4), new(5));
+        private record PermutationIndices(
+            int ColorIndex = 0,
+            int NationalityIndex = 0,
+            int PetIndex = 0,
+            int DrinkIndex = 0,
+            int SmokeIndex = 0);
 
-        public readonly Hypothesis[] Hypotheses = new[] { H1, H2, H3, H4, H5 };
+        public static Solution BuildInitial() => new Solution();
 
-        private const int CombinationsPerProp = 120; // 5 ^ 5
+        public int Level { get; init; } = 0;
+        private PermutationIndices PIndices = new PermutationIndices();
+        private Hypothesis H1 = new(1);
+        private Hypothesis H2 = new(2);
+        private Hypothesis H3 = new(3);
+        private Hypothesis H4 = new(4);
+        private Hypothesis H5 = new(5);
 
-        private Solution? Set(int attIndex, int nextNumber)
+        public Hypothesis[] Hypotheses => new[] { H1, H2, H3, H4, H5 };
+
+        private Solution? Set(int hIndex, int pIndex)
         {
-            if (nextNumber >= CombinationsPerProp)
+            if (pIndex >= AllPermutations.PermutationsCount)
                 return null;
-            return new Solution(
-                Level,
-                numbersSetters[attIndex](Numbers, nextNumber),
-                hypothesisSetters[attIndex](H1, 0, nextNumber),
-                hypothesisSetters[attIndex](H2, 1, nextNumber),
-                hypothesisSetters[attIndex](H3, 2, nextNumber),
-                hypothesisSetters[attIndex](H4, 3, nextNumber),
-                hypothesisSetters[attIndex](H5, 4, nextNumber));
+            var hypothesisSetter = hypothesisSetters[hIndex];
+            var permutationSetter = permutationSetters[hIndex];
+            return this with {
+                H1 = hypothesisSetter(H1, 0, pIndex),
+                H2 = hypothesisSetter(H2, 1, pIndex),
+                H3 = hypothesisSetter(H3, 2, pIndex),
+                H4 = hypothesisSetter(H4, 3, pIndex),
+                H5 = hypothesisSetter(H5, 4, pIndex),
+                PIndices = permutationSetter(PIndices, pIndex)
+            };
         }
 
-        private Solution Init(int attIndex) => Set(attIndex, 0)!;
+        private Solution Init(int hIndex) => Set(hIndex, 0)!;
 
-        private Solution? Next(int attIndex) => Set(attIndex, numberGetters[attIndex](Numbers) + 1);
+        private Solution? Next(int hIndex) => Set(hIndex, permutationGetters[hIndex](PIndices) + 1);
 
-        private readonly Func<CombinationNumbers, int>[] numberGetters = new Func<CombinationNumbers, int>[] {
-            (numbers) => numbers.ColorIndex,
-            (numbers) => numbers.NationalityIndex,
-            (numbers) => numbers.PetIndex,
-            (numbers) => numbers.DrinkIndex,
-            (numbers) => numbers.SmokeIndex,
-        };
+        private static readonly Func<PermutationIndices, int>[] permutationGetters =
+            new Func<PermutationIndices, int>[] {
+                pIndices => pIndices.ColorIndex,
+                pIndices => pIndices.NationalityIndex,
+                pIndices => pIndices.PetIndex,
+                pIndices => pIndices.DrinkIndex,
+                pIndices => pIndices.SmokeIndex,
+            };
 
-        private readonly Func<CombinationNumbers, int, CombinationNumbers>[] numbersSetters = new Func<CombinationNumbers, int, CombinationNumbers>[] {
-            (numbers, i) => numbers with { ColorIndex = i },
-            (numbers, i) => numbers with { NationalityIndex = i },
-            (numbers, i) => numbers with { PetIndex = i },
-            (numbers, i) => numbers with { DrinkIndex = i },
-            (numbers, i) => numbers with { SmokeIndex = i },
-        };
+        private static readonly Func<PermutationIndices, int, PermutationIndices>[] permutationSetters =
+            new Func<PermutationIndices, int, PermutationIndices>[] {
+                (pIndices, pIndex) => pIndices with { ColorIndex = pIndex },
+                (pIndices, pIndex) => pIndices with { NationalityIndex = pIndex },
+                (pIndices, pIndex) => pIndices with { PetIndex = pIndex },
+                (pIndices, pIndex) => pIndices with { DrinkIndex = pIndex },
+                (pIndices, pIndex) => pIndices with { SmokeIndex = pIndex },
+            };
 
-        private readonly Func<Hypothesis, int, int, Hypothesis>[] hypothesisSetters = new Func<Hypothesis, int, int, Hypothesis>[] {
-            (Hypothesis h, int hIndex, int nextNumber) => h with { Color = AllPermutations.ColorsSets[nextNumber][hIndex] },
-            (Hypothesis h, int hIndex, int nextNumber) => h with { Nationality = AllPermutations.NationalitiesSets[nextNumber][hIndex] },
-            (Hypothesis h, int hIndex, int nextNumber) => h with { Pet = AllPermutations.PetsSets[nextNumber][hIndex] },
-            (Hypothesis h, int hIndex, int nextNumber) => h with { Drink = AllPermutations.DrinksSets[nextNumber][hIndex] },
-            (Hypothesis h, int hIndex, int nextNumber) => h with { Smoke = AllPermutations.SmokesSets[nextNumber][hIndex] },
-        };
+        private static readonly Func<Hypothesis, int, int, Hypothesis>[] hypothesisSetters =
+            new Func<Hypothesis, int, int, Hypothesis>[] {
+                (Hypothesis h, int hIndex, int pIndex) => h with { Color = AllPermutations.ColorsSets[pIndex][hIndex] },
+                (Hypothesis h, int hIndex, int pIndex) => h with { Nationality = AllPermutations.NationalitiesSets[pIndex][hIndex] },
+                (Hypothesis h, int hIndex, int pIndex) => h with { Pet = AllPermutations.PetsSets[pIndex][hIndex] },
+                (Hypothesis h, int hIndex, int pIndex) => h with { Drink = AllPermutations.DrinksSets[pIndex][hIndex] },
+                (Hypothesis h, int hIndex, int pIndex) => h with { Smoke = AllPermutations.SmokesSets[pIndex][hIndex] },
+            };
 
         public Solution InitColors() => Init(0);
-        public Solution? NextSetOfColors() => Next(0);
         public Solution InitNationalities() => Init(1);
-        public Solution? NextSetOfNationalities() => Next(1);
         public Solution InitPets() => Init(2);
-        public Solution? NextSetOfPets() => Next(2);
         public Solution InitDrinks() => Init(3);
-        public Solution? NextSetOfDrinks() => Next(3);
         public Solution InitSmokes() => Init(4);
+
+        public Solution? NextSetOfColors() => Next(0);
+        public Solution? NextSetOfNationalities() => Next(1);
+        public Solution? NextSetOfPets() => Next(2);
+        public Solution? NextSetOfDrinks() => Next(3);
         public Solution? NextSetOfSmokes() => Next(4);
     }
 }
