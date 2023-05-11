4d5a230d07e81b0e77d29a6fd502b0d051ec0620
Mariano Desanze <protronm@gmail.com>
2023-05-11 00:24:45 -0300

    Add Stopwatch to Program.cs

--- ZebraPuzzle/Program.cs
+++ ZebraPuzzle/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace ZebraPuzzle
+﻿using System.Diagnostics;
+
+namespace ZebraPuzzle
 {
     public class Program
     {
         public static void Main()
         {
+            var sw = Stopwatch.StartNew();
             var drinksWater = ZebraPuzzle.DrinksWater();
             Console.WriteLine($"drinksWater={drinksWater}");
             var ownsZebra = ZebraPuzzle.OwnsZebra();
             Console.WriteLine($"ownsZebra={ownsZebra}");
+            Console.WriteLine(sw.Elapsed);
         }
     }
 }
